95b3adc273ec00f2b9c63617392daddfeab9d56f
lollo <lorenzo.cavatorta.pr@gmail.com>
2016-11-24 08:08:38 +0000

    refactor

--- polygons/switcher/test_core.py
+++ polygons/switcher/test_core.py
@@ -2,12 +2,13 @@ from unittest import TestCase
 from core import Core
 class TestCore(TestCase):
 
+    def setUp(self):
+        self.switcher = Core()
+
     def test_read_port_in(self):
-        switcher = Core()
-        switcher.port_in.status = 1
-        self.assertTrue(switcher.port_in.read() == 1)
+        self.switcher.port_in.status = 1
+        self.assertTrue(self.switcher.port_in.read() == 1)
         
     def test_set_port_in(self):
-        switcher = Core()
-        switcher.port_in.set_on()
-        self.assertTrue(switcher.port_in.read() == 1)
+        self.switcher.port_in.set_on()
+        self.assertTrue(self.switcher.port_in.read() == 1)
