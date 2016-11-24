74839398a5e32841250429db7e139bf40482ccce
LorenzoCavatorta <lorenzo.cavatorta.pr@gmail.com>
2016-11-24 13:12:48 +0000

    refactor

--- polygons/switcher/core.py
+++ polygons/switcher/core.py
@@ -2,8 +2,16 @@ from port_in import PortIn
 from port_out import PortOut
 class Core(object):
 
+    
     def __init__(self):
+        def patching_port_in_set_on(self):
+            self.port_in._status = 1
+
         self.port_in = PortIn()
+        self.port_in.set_on=patching_port_in_set_on
         self.port_out = PortOut()
 
+
+
+        
         
