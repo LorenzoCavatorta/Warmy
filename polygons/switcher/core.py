a3c89e602a81cce7e32d3119ca3a07f48552b045
LorenzoCavatorta <lorenzo.cavatorta.pr@gmail.com>
2016-11-24 13:21:24 +0000

    monkey patching

--- polygons/switcher/core.py
+++ polygons/switcher/core.py
@@ -6,6 +6,7 @@ class Core(object):
     def __init__(self):
         def patching_port_in_set_on():
             self.port_in._status = 1
+            self.send_on_to_out()
 
         self.port_in = PortIn()
 #        import pdb; pdb.set_trace()
