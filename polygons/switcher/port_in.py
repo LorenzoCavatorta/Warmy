c911953d934fa48d07d0bf230afd503bf1fc2162
LorenzoCavatorta <lorenzo.cavatorta.pr@gmail.com>
2016-11-24 12:48:22 +0000

    added port out

--- polygons/switcher/port_in.py
+++ polygons/switcher/port_in.py
@@ -1,19 +1,6 @@
-class port(object):
+from port import Port
 
-    def __init__(self):
-        self._status = None
+class port(Port):
+    pass
 
-    def check_status(self):
-        if self._status not in (0, 1):
-            raise ValueError('Non-boolean value detected for port')
-        return self._status
-        
-
-    def read(self):
-        return self.check_status()
-
-    def set_on(self):
-        self._status = 1
-
-    def set_off(self):
-        self._status = 0
+    
