cf8d6befd3c624f2dd03b8501af78c64a96560c3
lollo <lorenzo.cavatorta.pr@gmail.com>
2016-11-24 08:34:40 +0000

    test

--- polygons/switcher/port_in.py
+++ polygons/switcher/port_in.py
@@ -1,10 +1,16 @@
 class port(object):
 
     def __init__(self):
-        self.status = None
+        self._status = None
+
+    def check_status(self):
+        if self._status not in (0, 1):
+            raise ValueError('Non-boolean value detected for port')
+        return self._status
+        
 
     def read(self):
-        return self.status
+        return self.check_status()
 
     def set_on(self):
-        self.status = 1
+        self._status = 1
--- polygons/switcher/test_core.py
+++ polygons/switcher/test_core.py
@@ -6,9 +6,17 @@ class TestCore(TestCase):
         self.switcher = Core()
 
     def test_read_port_in(self):
-        self.switcher.port_in.status = 1
+        self.switcher.port_in._status = 1
         self.assertTrue(self.switcher.port_in.read() == 1)
-        
+
+    def test_port_in_non_boolean(self):
+        self.switcher.port_in.status = 7
+        with self.assertRaises(ValueError):
+            self.switcher.port_in.read()
+
+
     def test_set_port_in(self):
         self.switcher.port_in.set_on()
         self.assertTrue(self.switcher.port_in.read() == 1)
+
+
