1d3bc903a2880763bf47e64422aeece292115f4b
lollo <lorenzo.cavatorta.pr@gmail.com>
2016-11-26 18:22:39 +0000

    test1

--- polygons/adapters/test_switcher_out_to_control_unit.py
+++ polygons/adapters/test_switcher_out_to_control_unit.py
@@ -3,7 +3,9 @@ from switcher_out_to_control_unit import SwitcherOutToControlUnit
 
 class can_communicate_with_control_unit(TestCase):
 
-    def test_can_read_port_out(self):
+    def test_can_read_port_in(self):
         adapter = SwitcherOutToControlUnit()
         adapter.port_in.set_on()
         self.assertTrue(adapter.port_in.read() == 1)
+
+    
