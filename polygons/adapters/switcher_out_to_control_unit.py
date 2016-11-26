10ae57d68c6b87ae475a43e5ba9d4b6e826e8329
lollo <lorenzo.cavatorta.pr@gmail.com>
2016-11-26 19:57:02 +0000

    new test (failing) and arduino core

--- polygons/adapters/switcher_out_to_control_unit.py
+++ polygons/adapters/switcher_out_to_control_unit.py
@@ -1,6 +1,23 @@
 from Warmy.polygons.switcher.port_out import PortOut as PortIn
+import serial
 
 class SwitcherOutToControlUnit(object):
 
     def __init__(self):
         self.port_in = PortIn()
+        
+    def read_port_in(self):
+        return self.port_in.read()
+        
+    def read_port_out(self):
+        pass
+
+    def set_port_out(self,port_status):
+        self.ser = serial.Serial('/dev/ttyACM1', 9600)
+        self.ser.write(b'6')
+        self.ser.write(b'6')
+
+'''import serial
+from test_switcher_out_to_control_unit import  SwitcherOutToControlUnit
+ser = serial.Serial('/dev/ttyACM1', 9600)
+'''
--- polygons/adapters/test_switcher_out_to_control_unit.py
+++ polygons/adapters/test_switcher_out_to_control_unit.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
 from switcher_out_to_control_unit import SwitcherOutToControlUnit
+from Warmy.polygons.switcher.port_out import PortOut as PortFromSwitcher
 
 class can_communicate_with_control_unit(TestCase):
 
-    def test_can_read_port_in(self):
-        adapter = SwitcherOutToControlUnit()
-        adapter.port_in.set_on()
-        self.assertTrue(adapter.port_in.read() == 1)
+    def setUp(self):
+        self.adapter = SwitcherOutToControlUnit()
+        self.switcher_out = PortFromSwitcher()
 
-    
+    def test_can_read_port_in(self):
+        self.switcher_out.set_on() #implementation_specific
+        self.assertTrue(self.adapter.read_port_in() == 1)
+
+    def test_can_write_port_out(self):
+        self.adapter.set_port_out(1)
+        self.assertTrue(self.adapter.read_port_out() == 1)
