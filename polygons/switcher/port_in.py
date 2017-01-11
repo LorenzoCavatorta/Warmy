a7629826b08e343e1d2abf5c8ba2ca4eee50c497
lollo <lorenzo.cavatorta.pr@gmail.com>
2017-01-11 22:27:24 +0000

    restart with service notion

--- polygons/switcher/port_in.py
+++
@@ -1,9 +0,0 @@
-from port import PortBoolean as Port
-
-class PortIn(Port):
-
-    def __init__(self):
-        self.file_location = '/tmp/port_in.status'
-        super().__init__(self.file_location)
-
-    
--- polygons/switcher/port_out.py
+++
@@ -1,9 +0,0 @@
-from .port import PortBoolean as Port
-
-class PortOut(Port):
-
-    def __init__(self):
-        self.file_location = '/tmp/port_out.status'
-        super().__init__(self.file_location)
-
-    
